3d6350a524236a54dbab10aaf43960c487a96df8
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2025-02-02 14:58:57 +0300

    Использование кешей сборки в CI [#505355 #530409]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,37 @@
 rootProject.name = "DevOps"
-buildCache {
-    remote<HttpBuildCache> {
-        url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
-        isAllowInsecureProtocol = true
-        isAllowUntrustedServer = true
-        isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
-        credentials {
-            username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
-            password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
+
+val remoteCacheUrl = System.getenv("GRADLE_REMOTE_CACHE_URL") ?: (extra.properties["GRADLE_REMOTE_CACHE_URL"] as? String)
+val remoteCachePush = System.getenv("GRADLE_REMOTE_CACHE_PUSH") ?: (extra.properties["GRADLE_REMOTE_CACHE_PUSH"] as? String)
+val remoteCacheUsername = System.getenv("GRADLE_REMOTE_CACHE_USERNAME") ?: (extra.properties["GRADLE_REMOTE_CACHE_USERNAME"] as? String)
+val remoteCachePassword = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD") ?: (extra.properties["GRADLE_REMOTE_CACHE_PASSWORD"] as? String)
+
+if (remoteCacheUrl != null) {
+    buildCache {
+        remote<HttpBuildCache> {
+            url = uri(remoteCacheUrl)
+            isAllowInsecureProtocol = true
+            isAllowUntrustedServer = true
+            isPush = remoteCachePush?.toBoolean() ?: false
+            credentials {
+                username = remoteCacheUsername
+                password = remoteCachePassword
+            }
         }
     }
 }
+
+if (remoteCacheUrl != null) {
+    buildCache {
+        remote<HttpBuildCache> {
+            url = uri(remoteCacheUrl)
+            isAllowInsecureProtocol = true
+            isAllowUntrustedServer = true
+            isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
+            credentials {
+                username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
+                password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
+            }
+        }
+    }
+}
+
